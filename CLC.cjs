adb65c1fa45ec8d36fb9a27a20091a590a3c2b7d
preetham39 <36740601+preetham39@users.noreply.github.com>
2018-10-25 10:44:50 -0700

    Create CLC.cjs

---
+++ CLC.cjs
@@ -0,0 +1,86 @@
+({
+   onfocus : function(component,event,helper){
+       $A.util.removeClass(component.find("mySpinner"), "slds-show");
+        var forOpen = component.find("searchRes");
+            $A.util.addClass(forOpen, 'slds-is-open');
+            $A.util.removeClass(forOpen, 'slds-is-close');
+        // Get Default 5 Records order by createdDate DESC  
+         var getInputkeyWord = '';
+         helper.searchHelper(component,event,getInputkeyWord);
+    },
+    onblur : function(component,event,helper){       
+   /*  if(! component.get("v.doNotClearSearchText")){
+        component.set(“v.SearchKeyWord”, null);
+    }
+    component.set(“v.listOfSearchRecords”, null );
+    var forclose = component.find(“searchRes”);
+    $A.util.addClass(forclose, ‘slds-is-close’);
+    $A.util.removeClass(forclose, ‘slds-is-open’);
+    },*/
+      component.set("v.listOfSearchRecords", null );
+        var forclose = component.find("searchRes");
+        $A.util.addClass(forclose, 'slds-is-close');
+        $A.util.removeClass(forclose, 'slds-is-open');
+    },
+    keyPressController : function(component, event, helper) {
+       // get the search Input keyword   
+         var getInputkeyWord = component.get("v.SearchKeyWord");
+       // check if getInputKeyWord size id more then 0 then open the lookup result List and 
+       // call the helper 
+       // else close the lookup result List part.   
+        if( getInputkeyWord.length > 0 ){
+             var forOpen = component.find("searchRes");
+               $A.util.addClass(forOpen, 'slds-is-open');
+               $A.util.removeClass(forOpen, 'slds-is-close');
+            helper.searchHelper(component,event,getInputkeyWord);
+        }
+        else{  
+             component.set("v.listOfSearchRecords", null ); 
+             var forclose = component.find("searchRes");
+               $A.util.addClass(forclose, 'slds-is-close');
+               $A.util.removeClass(forclose, 'slds-is-open');
+          }
+	},
+    
+  // function for clear the Record Selaction 
+    clear :function(component,event,helper){
+         var pillTarget = component.find("lookup-pill");
+         var lookuppillUp = component.find("lookuppillUp");
+         var lookUpTarget = component.find("lookupField"); 
+        
+         $A.util.addClass(pillTarget, 'slds-hide');
+         $A.util.removeClass(pillTarget, 'slds-show');
+        $A.util.removeClass(lookuppillUp, 'slds-hide');
+         $A.util.addClass(lookuppillUp, 'slds-show');
+        
+         $A.util.addClass(lookUpTarget, 'slds-show');
+         $A.util.removeClass(lookUpTarget, 'slds-hide');
+      
+         component.set("v.SearchKeyWord",null);
+         component.set("v.listOfSearchRecords", null );
+         component.set("v.selectedRecord", {} );   
+    },
+    
+  // This function call when the end User Select any record from the result list.   
+    handleComponentEvent : function(component, event, helper) {
+    // get the selected Account record from the COMPONENT event 	 
+       var selectedAccountGetFromEvent = event.getParam("recordByEvent");
+	   component.set("v.selectedRecord" , selectedAccountGetFromEvent); 
+       
+        var forclose = component.find("lookup-pill");
+        var lookuppillUp = component.find("lookuppillUp");
+           $A.util.addClass(forclose, 'slds-show');
+           $A.util.removeClass(forclose, 'slds-hide');
+         $A.util.removeClass(lookuppillUp, 'slds-show');
+           $A.util.addClass(lookuppillUp, 'slds-hide');
+  
+        var forclose = component.find("searchRes");
+           $A.util.addClass(forclose, 'slds-is-close');
+           $A.util.removeClass(forclose, 'slds-is-open');
+        
+        var lookUpTarget = component.find("lookupField");
+            $A.util.addClass(lookUpTarget, 'slds-hide');
+            $A.util.removeClass(lookUpTarget, 'slds-show');  
+      
+	},
+})
